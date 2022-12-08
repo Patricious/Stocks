bd75a936c5bc1474c7aa8b2d7367cc899880be33
Patricia Mulaudzi <32956237+Patricious@users.noreply.github.com>
2022-12-08 09:29:28 +0200

    first commit

---
+++ StockList/Models/Stock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApi.Models;
+
+public class Stock
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Stock")]
+
+    public string currency_code { get; set; }
+
+    public decimal industry { get; set; }
+
+    public string stock { get; set; }
+
+    public string value { get; set; }
+
+    public string date { get; set; }
+ 
+    public string currencycode { get; set; } 
+}
+
---
+++ StockList/Models/StockListDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StockList.Models;
+    
+public class StockListDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string StockCollectionName { get; set; } = null!;
+}
